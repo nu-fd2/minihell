b62fcc977f56632a553efbfbb67eb16d7391d64d
nu-fd2 <omar0madouni@gmail.com>
2025-09-20 01:50:40 +0100

    idk

--- ex/m_red/man_dog.c
+++ ex/m_red/man_dog.c
@@ -6,11 +6,168 @@
 /*   By: oel-mado <oel-mado@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/16 06:41:04 by oel-mado          #+#    #+#             */
-/*   Updated: 2025/09/19 15:44:05 by oel-mado         ###   ########.fr       */
+/*   Updated: 2025/09/20 01:50:03 by oel-mado         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/header.h"
+
+typedef struct s_expansion
+{
+    char	*input;
+    char	*output;
+    int		s_quote;
+    int		d_quote;
+    int		i_index;
+    int		o_index;
+    int		len;
+} t_expansion;
+
+void	init_expansion(t_expansion *exp, char *input)
+{
+	exp->input = input;
+	exp->len = ft_strlen(input);
+	exp->s_quote = 0;
+	exp->d_quote = 0;
+	exp->i_index = 0;
+	exp->o_index = 0;
+    exp->output = NULL;
+}
+
+char *get_env_value(t_env *env, const char *key) {
+    char *value = NULL;
+
+    while (env) {
+        if (!strcmp(env->key, key)) {
+            value = strdup(env->value);
+            break;
+        }
+        env = env->next;
+    }
+    return value;
+}
+
+int get_var_len(t_env *env, char *key) {
+    char *tmp_value;
+    int len;
+
+    tmp_value = get_env_value(env, key);
+    if (tmp_value) {
+        len = strlen(tmp_value);
+        free(tmp_value);
+        return len;
+    }
+    return 0;
+}
+
+void process_input(t_expansion *exp, t_env *env) {
+    char *tmp_key;
+    int key_len;
+    int start;
+
+    while (exp->input[exp->i_index]) {
+        
+        if (exp->input[exp->i_index] == '\'')
+            exp->s_quote = !exp->s_quote;
+        else if (exp->input[exp->i_index] == '\"')
+            exp->d_quote = !exp->d_quote;
+
+        if (!exp->s_quote && exp->input[exp->i_index] == '$') {
+            start = ++exp->i_index;
+            exp->len--;
+            key_len = 0;
+            while (exp->input[exp->i_index] &&
+                    (isalnum((unsigned char)exp->input[exp->i_index]) ||
+                    exp->input[exp->i_index] == '_')) {
+                exp->i_index++;
+                exp->len--;
+                key_len++;
+            }
+            if (key_len > 0) {
+                tmp_key = malloc(key_len + 1);
+                if (!tmp_key)
+                    return;
+                strncpy(tmp_key, exp->input + start, key_len);
+                tmp_key[key_len] = '\0';
+                exp->len += get_var_len(env, tmp_key);
+                free(tmp_key);
+            } else {
+                exp->len++;
+            }
+        }
+        else
+            exp->i_index++;
+    }
+    exp->i_index = 0;
+}
+
+void fill_output(t_expansion *exp, t_env *env) {
+    while (exp->input[exp->i_index]) {
+        char c = exp->input[exp->i_index];
+        
+        if (c == '\'' && !exp->d_quote)
+            exp->s_quote = !exp->s_quote;
+        else if (c == '"' && !exp->s_quote)
+            exp->d_quote = !exp->d_quote;
+            
+        if (!exp->s_quote && c == '$') {
+            int start = ++exp->i_index;
+            int key_len = 0;
+            while (exp->input[exp->i_index] && 
+                    (isalnum((unsigned char)exp->input[exp->i_index]) || 
+                    exp->input[exp->i_index] == '_')) {
+                exp->i_index++;
+                key_len++;
+            }
+            if (key_len > 0) {
+                char *tmp_key = malloc(key_len + 1);
+                if (tmp_key) {
+                    strncpy(tmp_key, exp->input + start, key_len);
+                    tmp_key[key_len] = '\0';
+                    char *val = get_env_value(env, tmp_key);
+                    if (val) {
+                        int vlen = strlen(val);
+                        memcpy(exp->output + exp->o_index, val, vlen);
+                        exp->o_index += vlen;
+                        free(val);
+                    }
+                    free(tmp_key);
+                }
+            } else {
+                exp->output[exp->o_index++] = '$';
+            }
+        } else
+            exp->output[exp->o_index++] = exp->input[exp->i_index++];
+    }
+    exp->output[exp->o_index] = '\0';
+}
+
+
+char *expand_env_vars(char *raw_line, t_env *env) {
+    t_expansion expansion;
+
+    init_expansion(&expansion, raw_line);
+
+    process_input(&expansion, env);
+    
+    expansion.output = malloc(expansion.len + 1);
+    if (!expansion.output)
+        return NULL;
+
+    expansion.i_index = 0;
+    expansion.o_index = 0;
+    expansion.s_quote = 0;
+    expansion.d_quote = 0;
+    
+    
+    fill_output(&expansion, env);
+
+    free(expansion.input);
+    return expansion.output;
+}
+
+
+
 
 void exer(t_data *data, t_short *shart, int stat)
 {
@@ -33,6 +190,8 @@ void f_dog_pip(t_data *data, t_short *ts, int p_in)
     s = get_next_line(p_in);
     while (s)
     {
+        
+        s = expand_env_vars(s, data->env);
         ft_putstr_fd(s, ts->pip[1]);
         free(s);
         s = get_next_line(p_in);
@@ -112,7 +271,7 @@ int frk_dog(t_data *data, t_short *ts)
     else if (lil_vro == 0)
     {
         signal(SIGINT, SIG_DFL);
-        // data->exm = 1;
+        
         data->exm = hot_dog(data, ts);
         exer(data, ts, 1);
     }
@@ -128,7 +287,7 @@ int frk_dog(t_data *data, t_short *ts)
         }
 		else
 			data->exm = 0;
-        // data->exm = stat;
+        
     }
     return 0;
 }
